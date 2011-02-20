ba73acb516dfc42a30f8022147878188c64b37a8
Scott <Scott@dell8100>
2011-02-19 20:13:38 -0500

    Add firepit search test

--- src/main/java/com/examples/cabin/TestDataGenerator.java
+++ src/main/java/com/examples/cabin/TestDataGenerator.java
@@ -72,67 +72,6 @@ public class TestDataGenerator extends AbstractPageBean {
 						"sample", 2));
 		cabin3.setFirePit(true);
 		testData.add(cabin3);
-		// testData.add(new Cabin("Cedar Hill Lodge", new Address("123 Main St",
-		// "New Lexington", State.OH,new LatLng(40,-82)),new RentalTerms(new
-		// Integer(10), new BigDecimal(100), new BigDecimal(10), new Integer(4),
-		// null),new Review("sample",2)));
-		// testData.add(new Cabin("Heritage Retreat", new Address("123 Main St",
-		// "New Lexington", State.OH,new LatLng(40,-82)),new RentalTerms(new
-		// Integer(10), new BigDecimal(100), new BigDecimal(10), new Integer(4),
-		// null),new Review("sample",2)));
-		// testData.add(new Cabin("Timber Creek", new Address("123 Main St",
-		// "New Lexington", State.OH,new LatLng(40,-82)),new RentalTerms(new
-		// Integer(10), new BigDecimal(100), new BigDecimal(10), new Integer(4),
-		// null),new Review("sample",2)));
-		// testData.add(new Cabin("Luc Lodge", new Address("123 Main St",
-		// "New Lexington", State.OH,new LatLng(40,-82)),new RentalTerms(new
-		// Integer(10), new BigDecimal(100), new BigDecimal(10), new Integer(4),
-		// null),new Review("sample",2)));
-		// testData.add(new Cabin("Serenity", new Address("123 Main St",
-		// "New Lexington", State.OH,new LatLng(40,-82)),new RentalTerms(new
-		// Integer(10), new BigDecimal(100), new BigDecimal(10), new Integer(4),
-		// null),new Review("sample",2)));
-		// testData.add(new Cabin("Hillside Lodge", new Address("123 Main St",
-		// "New Lexington", State.OH,new LatLng(40,-82)),new RentalTerms(new
-		// Integer(10), new BigDecimal(100), new BigDecimal(10), new Integer(4),
-		// null),new Review("sample",2)));
-		// testData.add(new Cabin("Garleff Lodge", new Address("123 Main St",
-		// "New Lexington", State.OH,new LatLng(40,-82)),new RentalTerms(new
-		// Integer(10), new BigDecimal(100), new BigDecimal(10), new Integer(4),
-		// null),new Review("sample",2)));
-		// testData.add(new Cabin("Black Ridge", new Address("123 Main St",
-		// "New Lexington", State.OH,new LatLng(40,-82)),new RentalTerms(new
-		// Integer(10), new BigDecimal(100), new BigDecimal(10), new Integer(4),
-		// null),new Review("sample",2)));
-		// testData.add(new Cabin("Indian Mound Lodge", new
-		// Address("123 Main St", "New Lexington", State.OH,new
-		// LatLng(40,-82)),new RentalTerms(new Integer(10), new BigDecimal(100),
-		// new BigDecimal(10), new Integer(4), null),new Review("sample",2)));
-		// testData.add(new Cabin("Autumn Ridge", new Address("123 Main St",
-		// "New Lexington", State.OH,new LatLng(40,-82)),new RentalTerms(new
-		// Integer(10), new BigDecimal(100), new BigDecimal(10), new Integer(4),
-		// null),new Review("sample",2)));
-		// testData.add(new Cabin("Crockets run - William Travis", new
-		// Address("123 Main St", "New Lexington", State.OH,new
-		// LatLng(40,-82)),new RentalTerms(new Integer(10), new BigDecimal(100),
-		// new BigDecimal(10), new Integer(4), null),new Review("sample",2)));
-		// testData.add(new Cabin("Westwind", new Address("123 Main St",
-		// "New Lexington", State.OH,new LatLng(40,-82)),new RentalTerms(new
-		// Integer(10), new BigDecimal(100), new BigDecimal(10), new Integer(4),
-		// null),new Review("sample",2)));
-		// testData.add(new Cabin("Maple Forest Lodge", new
-		// Address("123 Main St", "New Lexington", State.OH,new
-		// LatLng(40,-82)),new RentalTerms(new Integer(10), new BigDecimal(100),
-		// new BigDecimal(10), new Integer(4), null),new Review("sample",2)));
-		// testData.add(new Cabin("Hocking Hills Getaway", new
-		// Address("123 Main St", "New Lexington", State.OH,new
-		// LatLng(40,-82)),new RentalTerms(new Integer(10), new BigDecimal(100),
-		// new BigDecimal(10), new Integer(4), null),new Review("sample",2)));
-		// testData.add(new Cabin("Dogwood", new Address("123 Main St",
-		// "New Lexington", State.OH,new LatLng(40,-82)),new RentalTerms(new
-		// Integer(10), new BigDecimal(100), new BigDecimal(10), new Integer(4),
-		// null),new Review("sample",2)));
-
 		testData.add(new Cabin("Someplace in PA", new Address("123 Main St",
 				"Reading", State.PA, "43152", 40.0, -80.0), null, null));
 	}
@@ -140,14 +79,6 @@ public class TestDataGenerator extends AbstractPageBean {
 	@Transactional(rollback=false)
 	public void loadTestData() {
 		
-//		boolean transactionStarted = false;
-//		try {
-//			db.joinTransaction(); 
-//		} catch (javax.persistence.TransactionRequiredException e) {
-//			userTransaction.begin();
-//			db.joinTransaction();
-//			transactionStarted = true;
-//		}
 		if (getNumberOfCabins() > 0) {
 			deleteExistingCabins();
 		}
@@ -155,12 +86,7 @@ public class TestDataGenerator extends AbstractPageBean {
 		for (Cabin cabin : testData) {
 			db.persist(cabin);
 		}
-//		if (transactionStarted) {
-//			userTransaction.commit();
-//		}
 		log.info("Test Data Loaded.");
-		//addError("msgs", "Ok", "Test Data Added.");
-
 	}
 
 	@Transactional
--- src/main/java/com/examples/cabin/entity/Cabin_.java
+++ src/main/java/com/examples/cabin/entity/Cabin_.java
@@ -5,7 +5,7 @@ import javax.persistence.metamodel.ListAttribute;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="Dali", date="2011-02-19T05:35:22.112-0500")
+@Generated(value="Dali", date="2011-02-19T19:15:49.485-0500")
 @StaticMetamodel(Cabin.class)
 public class Cabin_ extends AbstractEntity_ {
 	public static volatile SingularAttribute<Cabin, String> name;
@@ -14,9 +14,9 @@ public class Cabin_ extends AbstractEntity_ {
 	public static volatile SingularAttribute<Cabin, Boolean> hotTub;
 	public static volatile SingularAttribute<Cabin, Boolean> firePit;
 	public static volatile SingularAttribute<Cabin, Boolean> firePlace;
+	public static volatile SingularAttribute<Cabin, String> phoneNumber;
 	public static volatile SingularAttribute<Cabin, Address> address;
 	public static volatile SingularAttribute<Cabin, RentalTerms> rentalTerms;
 	public static volatile ListAttribute<Cabin, Review> reviews;
 	public static volatile ListAttribute<Cabin, Bedroom> bedrooms;
-	public static volatile SingularAttribute<Cabin, String> phoneNumber;
 }
